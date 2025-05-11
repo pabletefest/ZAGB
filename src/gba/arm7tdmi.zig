baba6b9ebe975923b85ca61d3e6f97908fbda319
PabloSF <62628314+pabletefest@users.noreply.github.com>
2025-05-12 01:40:19 +0200

    Added ProgramStatusRegister type (packed bitfields)

--- src/gba/arm7tdmi.zig
+++ src/gba/arm7tdmi.zig
@@ -1,8 +1,64 @@
 const std = @import("std");
 
-const CpuStateMode = enum {
+const CpuStateMode = enum(u1) {
     ARM = 0,
     THUMB = 1,
+};
+
+const ProgramStatusRegister = packed struct(u32) {
+    mode_bits: enum(u5) {
+        old_user = 0,
+        old_fiq = 1,
+        old_irq = 2,
+        old_supervisor = 3,
+        user = 16,
+        fiq = 17,
+        irq = 18,
+        supervisor = 19,
+        abort = 23,
+        undef = 27,
+        system = 31,
+    },
+    state_bit: enum(u1) {
+        arm = 0,
+        thumb = 1,
+    },
+    fiq_disable: enum(u1) {
+        enable = 0,
+        disable = 1,
+    },
+    irq_disable: enum(u1) {
+        enable = 0,
+        disable = 1,
+    },
+    unused_abort_disable: u1,
+    unused_endian: u1,
+    reserved_1: u14,
+    unused_jazelle_mode: enum(u1) {
+        none = 0,
+        jazelle_bytecode = 1,
+    },
+    reserved_2: u2,
+    sticky_overflow: enum(u1) {
+        none = 0,
+        sticky_overflow = 1,
+    },
+    overflow_flag: enum(u1) {
+        no_overflow = 0,
+        overflow = 1,
+    },
+    carry_flag: enum(u1) {
+        no_carry = 0,
+        carry = 1,
+    },
+    zero_flag: enum(u1) {
+        not_zero = 0,
+        zero = 1,
+    },
+    sign_flag: enum(u1) {
+        not_signed = 0,
+        signed = 1,
+    },
 };
 
 var CPU = struct {
