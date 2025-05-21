616279ebffe122a2d15f65e88ed5e405c40060bf
PabloSF <62628314+pabletefest@users.noreply.github.com>
2025-05-21 20:41:38 +0200

    Implemented entering operation mode in cpu core

--- src/gba/arm7tdmi.zig
+++ src/gba/arm7tdmi.zig
@@ -5,6 +5,32 @@ const CpuStateMode = enum(u1) {
     THUMB = 1,
 };
 
+const OperationModes = enum(u5) {
+    OLD_USER = 0,
+    OLD_FIQ = 1,
+    OLD_IRQ = 2,
+    OLD_SUPERVISOR = 3,
+    USER = 16,
+    FIQ = 17,
+    IRQ = 18,
+    SUPERVISOR = 19,
+    ABORT = 23,
+    UNDEFINED = 27,
+    SYSTEM = 31,
+
+    fn getRegBankOfMode(self: OperationModes) u5 {
+        return switch (self) {
+            .USER, .SYSTEM => 0,
+            .FIQ => 1,
+            .IRQ => 2,
+            .SUPERVISOR => 3,
+            .ABORT => 4,
+            .UNDEFINED => 5,
+            else => unreachable,
+        };
+    }
+};
+
 const CPSRBitsMask = enum(u32) {
     N = 0x80000000,
     Z = 0x40000000,
@@ -16,23 +42,8 @@ const CPSRBitsMask = enum(u32) {
 };
 
 const ProgramStatusRegister = packed struct(u32) {
-    mode_bits: enum(u5) {
-        old_user = 0,
-        old_fiq = 1,
-        old_irq = 2,
-        old_supervisor = 3,
-        user = 16,
-        fiq = 17,
-        irq = 18,
-        supervisor = 19,
-        abort = 23,
-        undef = 27,
-        system = 31,
-    },
-    state_bit: enum(u1) {
-        arm = 0,
-        thumb = 1,
-    },
+    mode_bits: OperationModes,
+    state_bit: CpuStateMode,
     fiq_disable: enum(u1) {
         enable = 0,
         disable = 1,
@@ -102,38 +113,82 @@ const PCIncrement = enum(u3) {
 
 const CPU = struct {
     gpr: [16]u32,
-    banked_regs: [7][7]u32, // we bank regs 8-14 included, to simplify the process
+    banked_regs: [6][7]u32, // we bank regs 8-14 included, to simplify the process
     cpsr: ProgramStatusRegister,
-    spsr: [7]ProgramStatusRegister,
+    spsr: [6]ProgramStatusRegister,
     pipeline: [2]u32,
 
+    const SP = 13;
+    const LR = 14;
+    const PC = 15;
+
     pub fn init() CPU {
-        return CPU{
+        var cpu = CPU{
             .gpr = std.mem.zeroes([16]u32),
-            .banked_regs = std.mem.zeroes([7][7]u32),
+            .banked_regs = std.mem.zeroes([6][7]u32),
             .cpsr = std.mem.zeroes(ProgramStatusRegister),
-            .spsr = std.mem.zeroes([7]ProgramStatusRegister),
+            .spsr = std.mem.zeroes([6]ProgramStatusRegister),
             .pipeline = std.mem.zeroes([2]u32),
         };
+
+        cpu.cpsr.mode_bits = OperationModes.SUPERVISOR;
+        cpu.cpsr.state_bit = CpuStateMode.ARM;
+
+        return cpu;
     }
 
     fn getPC(self: CPU) u32 {
-        return self.gpr[15];
+        return self.gpr[PC];
     }
 
     fn setPC(self: *CPU, value: u32) void {
-        self.gpr[15] = value;
+        self.gpr[PC] = value;
     }
 
     fn incrementPC(self: *CPU, increment: PCIncrement) void {
-        self.gpr[15] +%= @intFromEnum(increment);
+        self.gpr[PC] +%= @intFromEnum(increment);
+    }
+
+    fn enterOperationMode(self: *CPU, new_mode: OperationModes) void {
+        const banked_regs_offset = 8; // HI registers [8, 12]
+
+        const prev_mode = self.cpsr.mode_bits;
+        const prev_bank = prev_mode.getRegBankOfMode();
+        const new_bank = new_mode.getRegBankOfMode();
+
+        if (prev_mode == .FIQ) {
+            for (8..SP) |index| {
+                self.banked_regs[prev_bank][index - banked_regs_offset] = self.gpr[index];
+                self.gpr[index] = self.banked_regs[OperationModes.USER.getRegBankOfMode()][index - banked_regs_offset];
+            }
+        } else if (new_mode == .FIQ) {
+            for (8..SP) |index| {
+                self.banked_regs[OperationModes.USER.getRegBankOfMode()][index - banked_regs_offset] = self.gpr[index];
+                self.gpr[index] = self.banked_regs[new_bank][index - banked_regs_offset];
+            }
+        }
+
+        self.banked_regs[prev_bank][SP - banked_regs_offset] = self.gpr[SP];
+        self.banked_regs[prev_bank][LR - banked_regs_offset] = self.gpr[LR];
+
+        self.gpr[SP] = self.banked_regs[new_bank][SP - banked_regs_offset];
+        self.gpr[LR] = self.banked_regs[new_bank][LR - banked_regs_offset];
+
+        if (prev_mode != .USER or prev_mode != .SYSTEM) {
+            self.spsr[prev_bank] = self.cpsr;
+            self.cpsr = self.spsr[new_bank];
+        }
+
+        self.cpsr.mode_bits = new_mode;
     }
 };
 
 test "CPU init" {
     var cpu = CPU.init();
 
-    try std.testing.expectEqual(cpu.cpsr.getRegRaw(), 0);
+    const cpsr_init: u6 = 19 | (0 << 5); // Supervisor and ARM modes
+
+    try std.testing.expectEqual(cpu.cpsr.getRegRaw(), cpsr_init);
 
     cpu.cpsr.setRegRaw(100);
 
@@ -170,4 +225,12 @@ test "PC register operations" {
     cpu.incrementPC(PCIncrement.THUMB_INC);
 
     try std.testing.expectEqual(cpu.getPC(), 0x00000001);
+}
+
+test "Enter operation mode" {
+    var cpu = CPU.init();
+
+    cpu.enterOperationMode(OperationModes.FIQ);
+
+    try std.testing.expectEqual(OperationModes.FIQ, cpu.cpsr.mode_bits);
 }
